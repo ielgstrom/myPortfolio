d1e7a2dc99fbf95b48a1aee2065d37a7324afac2
Ignasi Elgström <i.elgstrom@gmail.com>
2024-02-26 00:39:43 +0100

    final touches

--- src/components/Ceràmica.jsx
+++ src/components/Ceràmica.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Ceramica from '../dataImages.json';
 import Card from './Card';
+import '../index.css';
 
 const Ceràmica = () => {
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbarOverlay = document.querySelector('.navbar-overlay');
+      if (window.scrollY > 0 && window.scrollY < 40) {
+        navbarOverlay.style.display = 'block';
+      } else {
+        navbarOverlay.style.display = 'none';
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <main className='row justify-content-around'>
+    <main className={`row justify-content-around content`}>
+    
     {Ceramica.ceramica.map(elem=>
-    <Card urlImage={elem.url} title={elem.title} descr={elem.description} o/>
+    <Card urlImage={elem.url} title={elem.title} descr={elem.description}/>
     )}
     </main>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <nav className='navbar sticky-top row justify-content-center'>
-    <div className='text-center col-1 '>Ceràmica</div>
-    <div className='text-center col-1'>Simulaciones</div>
-    <div className='text-center col-1'>Render</div>
-    <div className='text-center col-1'>CV</div>
+    <nav className='navbar sticky-top row justify-content-center' style={{zIndex:10}}>
+      <div className='text-center mx-2 col-1 '>Ceràmica</div>
+      <div className='text-center mx-2 col-1'>Simulaciones</div>
+      <div className='text-center mx-2 col-1'>Render</div>
+      <div className='text-center mx-2 col-1'>CV</div>
+      <div className="navbar-overlay"></div> 
+      
     </nav>
   )
 }
